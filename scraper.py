09641277ac075187d736e5a5577f913640c3b6c5
igorcav16 <84544994+igorcav16@users.noreply.github.com>
2026-05-08 11:40:33 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,39 +11,35 @@ url = (
     f"apikey={API_KEY}"
     f"&url={TARGET_URL}"
     "&js_render=true"
+    "&wait=15000"
+    "&premium_proxy=true"
 )
 
 response = requests.get(url, timeout=120)
 
 html = response.text
 
-print("HTML recebido")
-print(html[:3000])
+print("HTML carregado")
 
-taxa = None
-
-# procura qualquer ocorrência do título
-padrao_titulo = r'Tesouro Renda\+ Aposentadoria Extra 2065'
-
-titulo = re.search(
-    padrao_titulo,
+# procura trecho do Renda+
+match_titulo = re.search(
+    r'Tesouro Renda\+ Aposentadoria Extra 2065',
     html,
     re.S
 )
 
-if titulo:
+taxa = None
+
+if match_titulo:
 
     print("Título encontrado")
 
-    # pega trecho próximo do título
-    inicio = titulo.start()
+    inicio = match_titulo.start()
 
-    trecho = html[inicio:inicio + 5000]
+    trecho = html[inicio:inicio + 10000]
 
-    print("TRECHO:")
     print(trecho)
 
-    # procura IPCA +
     taxa_match = re.search(
         r'IPCA\s*\+\s*([0-9,]+)',
         trecho,
