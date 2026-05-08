34772ae0b6594582170f0e20fc6fe1d0b6604563
igorcav16 <84544994+igorcav16@users.noreply.github.com>
2026-05-08 10:38:26 -0300

    Update scraper.py

--- .github/workflows/scraper.py
+++ .github/workflows/scraper.py
@@ -1,27 +1,42 @@
-import requests
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 import json
 import re
 
-url = "https://www.tesourodireto.com.br/titulos/precos-e-taxas.htm"
-
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-html = requests.get(url, headers=headers).text
-
-# procura taxa do Renda+ 2065
-padrao = r'Renda\+\sAposentadoria\sExtra\s2065.*?IPCA \+ ([0-9,]+)%'
-
-resultado = re.search(padrao, html, re.S)
+URL = "https://www.tesourodireto.com.br/titulos/precos-e-taxas.htm"
 
 taxa = None
 
-if resultado:
-    taxa = float(
-        resultado.group(1).replace(",", ".")
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(
+        headless=True
     )
+
+    page = browser.new_page()
+
+    page.goto(URL, timeout=120000)
+
+    page.wait_for_timeout(10000)
+
+    html = page.content()
+
+    browser.close()
+
+    # procura taxa do Renda+ 2065
+    padrao = r'Renda\+\sAposentadoria\sExtra\s2065.*?IPCA \+ ([0-9,]+)%'
+
+    resultado = re.search(
+        padrao,
+        html,
+        re.S
+    )
+
+    if resultado:
+
+        taxa = float(
+            resultado.group(1)
+            .replace(",", ".")
+        )
 
 dados = {
     "taxa": taxa
